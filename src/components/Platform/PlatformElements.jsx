69ae11de7eeaf03b33ea132a65735132c5099b90
pujiragil <ragilsg164@gmail.com>
2023-05-14 22:22:40 +0700

    created PlatformImage component and replace existing image

--- src/components/Platform/PlatformElements.jsx
+++ src/components/Platform/PlatformElements.jsx
@@ -1,28 +1,25 @@
-import triangle from '../../assets/triangle.svg'
-import coursera from '../../assets/coursera.svg'
-import edx from '../../assets/edx.svg'
-import udemy from '../../assets/udemy.svg'
+import triangle from "../../assets/triangle.svg";
+
+export const PlatformImage = ({ src, alt, width, height, isLazy }) => {
+  return (
+    <img
+      src={src}
+      alt={alt}
+      width={width}
+      height={height}
+      loading={isLazy ? "lazy" : "eager"}
+      className="w-full h-auto"
+    />
+  );
+};
 
 export const Triangle = () => {
   return (
-    <img className="absolute z-10 -top-12 right-9 w-24 md:w-40 md:absolute md:-top-[90px] lg:absolute lg:w-48 lg:-top-28" src={triangle} alt="triangle" />
-  )
-}
+    <img
+      className="absolute z-10 -top-12 right-9 w-24 md:w-40 md:absolute md:-top-[90px] lg:absolute lg:w-48 lg:-top-28"
+      src={triangle}
+      alt="triangle"
+    />
+  );
+};
 
-export const Edx = () => {
-  return (
-    <img className="relative h-8 md:h-auto lg:h-auto w-12 md:w-28 lg:w-36 object-fill" src={edx} alt="edx" />
-  )
-}
-
-export const Coursera = () => {
-  return (
-    <img className="relative h-3 md:h-auto lg:h-auto w-20 lg:w-48 md:w-32 object-fill" src={coursera} alt="coursera" />
-  )
-}
-
-export const Udemy = () => {
-  return (
-    <img className="relative h-8 md:h-auto lg:h-auto w-20 md:w-28 lg:w-44 object-fill" src={udemy} alt="udemy" />
-  )
-}
--- src/components/Platform/index.jsx
+++ src/components/Platform/index.jsx
@@ -1,15 +1,54 @@
-import React from 'react'
-import { Coursera, Edx, Triangle, Udemy } from './PlatformElements'
+import coursera from "../../assets/coursera.svg";
+import edx from "../../assets/edx.svg";
+import udemy from "../../assets/udemy.svg";
+import {
+  PlatformImage,
+  Triangle,
+} from "./PlatformElements";
+
+const platforms = [
+  {
+    id: "edx",
+    src: edx,
+    alt: "edx-icon",
+    width: 51,
+    height: 31,
+    isLazy: true,
+  },
+  {
+    id: "udemy",
+    src: udemy,
+    alt: "udemy-icon",
+    width: 84,
+    height: 32,
+    isLazy: true,
+  },
+  {
+    id: "coursera",
+    src: coursera,
+    alt: "coursera-icon",
+    width: 83,
+    height: 13,
+    isLazy: true,
+  },
+];
 
 const Platform = () => {
   return (
-    <div className="relative flex justify-evenly h-48 lg:72 w-full items-center">
-      <Triangle/>
-      <Edx/>
-      <Udemy/>
-      <Coursera/>
+    <div className="relative grid grid-cols-3 h-48 lg:h-72 w-full place-items-center gap-14 p-6 md:gap-28 lg:gap-[154px]">
+      <Triangle />
+      {platforms.map((platform) => (
+        <PlatformImage
+          key={platform.id}
+          src={platform.src}
+          alt={platform.alt}
+          width={platform.width}
+          height={platform.height}
+          isLazy={platform.isLazy}
+        />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Platform
+export default Platform;

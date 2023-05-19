f0a10904e00c94cb18c1307f65d65bca220c1c33
pujiragil <ragilsg164@gmail.com>
2023-05-19 20:06:08 +0700

    create cardPrimary component

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,17 +1,53 @@
-import { CardHeader, CardItem } from "./CardElements"
-import { cardOne, cardThree, cardTwo } from "../../data/card/cardObj"
+import React from "react";
+import Layout from "../Layout";
+import { Heading, Paragraph } from "../GlobalElements";
 
-const Card = () => {
+const CardPrimary = () => {
   return (
-    <div className="px-4 mb-20 lg:mb-40">
-      <CardHeader />
-      <div className="flex flex-col lg:flex-row lg:gap-x-8 justify-center items-center">
-        <CardItem {...cardOne} />
-        <CardItem {...cardTwo} />
-        <CardItem {...cardThree} />
-      </div>
-    </div>
-  )
-}
+    <Layout bg="bg-white" padding={["px-5", "py-10", "md:px-6", "md:py-12"]}>
+      <div className="grid gap-20 place-items-center lg:grid-cols-2">
+        <div className="space-y-8 lg:order-2">
+          <div className="space-y-5">
+            <Heading
+              type="h2"
+              widths={["w-full", "sm:w-4/5"]}
+              text="The language for building web pages"
+            />
+            <Paragraph
+              isPrimary={false}
+              widths={["w-full", "sm:w-4/5", "md:w-8/12", "lg:w-9/12"]}
+              text="Go live on a fast, reliable, and hassle-free hosting network that scales with your business with one click and go code!"
+            />
+          </div>
+          <button className="px-6 py-3 flex items-center gap-3 font-poppins font-semibold bg-black text-white rounded md:text-xl">
+            Try it Yourself
+            <img
+              className="w-8 h-auto md:w-10"
+              src="/assets/arrow-button.svg"
+              alt="arrow-button"
+            />
+          </button>
+        </div>
 
-export default Card
+        <div className="relative w-full flex justify-center sm:w-4/5 md:w-3/4 lg:order-1 lg:w-full">
+          <img className="w-full" src="/assets/code2.webp" alt="code" />
+          <img
+            className="w-28 h-auto object-cover absolute bottom-0 right-0 md:w-40 lg:w-32"
+            src="/assets/triangle.svg"
+            alt="triangle"
+          />
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+const CardSection = () => {
+  return (
+    <>
+      <CardPrimary />
+    </>
+  );
+};
+
+export default CardSection;

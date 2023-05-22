326f7ab38d9d469cd0258c27c304132899a4ecbc
pujiragil <ragilsg164@gmail.com>
2023-05-22 16:20:27 +0700

    added new element for h4

--- src/components/GlobalElements.jsx
+++ src/components/GlobalElements.jsx
@@ -3,7 +3,7 @@ export const Heading = ({ type, text, widths }) => {
     case "h1": {
       return (
         <h1
-          className={`font-source font-medium text-black text-[34px] leading-10 md:text-6xl md:leading-[75px] ${
+          className={`font-source text-[34px] font-medium leading-10 text-black md:text-6xl md:leading-[75px] ${
             widths?.length ? widths.join(" ") : ""
           }`}
         >
@@ -15,7 +15,7 @@ export const Heading = ({ type, text, widths }) => {
     case "h2": {
       return (
         <h2
-          className={`font-poppins font-bold text-black text-[34px] leading-10 tracking-[0.25px] md:text-5xl md:leading-normal ${
+          className={`font-poppins text-[34px] font-bold leading-10 tracking-[0.25px] text-black md:text-5xl md:leading-normal ${
             widths?.length ? widths.join(" ") : ""
           }`}
         >
@@ -27,12 +27,22 @@ export const Heading = ({ type, text, widths }) => {
     case "h3": {
       return (
         <h3
-          className={`font-poppins font-bold text-black text-2xl tracking-[0.25px] md:text-[34px] ${
+          className={`font-poppins text-2xl font-bold tracking-[0.25px] text-black md:text-[34px] ${
             widths?.length ? widths.join(" ") : ""
           }`}
         >
           {text}
         </h3>
+      );
+    }
+
+    case "h4": {
+      return (
+        <h4
+          className={`font-poppins text-xl font-medium tracking-[0.25px] text-black md:text-3xl md:font-bold ${
+            widths?.length ? widths?.join(" ") : ""
+          }`}
+        ></h4>
       );
     }
   }
@@ -54,7 +64,7 @@ export const Paragraph = ({ isPrimary, sizes, weights, widths, text }) => {
         sizes,
         weights,
         widths
-      )} text-black leading-5`}
+      )} leading-5 text-black`}
     >
       {text}
     </p>

e02974d6ceea25cbe488478f14154b26daf6e774
pujiragil <ragilsg164@gmail.com>
2023-05-22 22:58:23 +0700

    added accordion transition

--- src/components/Faq/index.jsx
+++ src/components/Faq/index.jsx
@@ -1,86 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../Layout";
-import { FaMinus, FaPlus } from "react-icons/fa";
-import { useState } from "react";
-// import { FaqWrapper } from "./FaqElements";
-import { Heading } from "../GlobalElements";
+import { Heading, Paragraph } from "../GlobalElements";
 
 const FaqDescription = ({ isOpen, description }) => {
   return (
     <div
-      className={`${
+      className={`grid ${
         isOpen ? "grid-rows-[1fr]" : "grid-rows-[0fr]"
-      } grid transition-all duration-300 ease-in`}
+      } transition-all duration-300 ease-in`}
     >
-      <p>{description}</p>
+      <Paragraph isPrimary={false} text={description} />
     </div>
   );
 };
 
-const FaqSection = ({ isOpen, title, description }) => {
-  const [open, setOpen] = useState(false);
-
-  const clickOpen = () => {
-    setOpen(!open);
-  };
-
+const FaqSection = ({ isOpen, handleOpen, title, description }) => {
   return (
-    <div className="grid divide-y divide-[#E4E4E4]">
-      <div className="grid grid-cols-[1fr_32px]">
-        <div>
-          <Heading type="h4" text={title} />
-          <p>{description}</p>
-        </div>
-        <div>
-          <button
-            className={`h-8 w-8 rounded-full font-poppins text-2xl font-medium ${
-              isOpen
-                ? "bg-[#DDF247] text-black"
-                : "border border-black text-[#DDF247]"
-            }`}
-          >
-            {isOpen ? "-" : "+"}
-          </button>
-        </div>
+    <div className="grid grid-cols-[1fr_32px] py-10 first:pt-0 last:pb-0">
+      <div className="space-y-4">
+        <Heading type="h4" text={title} />
+        <FaqDescription isOpen={isOpen} description={description} />
+      </div>
+      <div>
+        <button
+          onClick={handleOpen}
+          className={`flex h-8 w-8 items-center justify-center rounded-full font-poppins text-2xl font-medium transition-all duration-300 ease-in ${
+            isOpen
+              ? "rotate-0 bg-[#DDF247] text-black"
+              : "rotate-90 border border-black text-[#DDF247]"
+          }`}
+        >
+          {isOpen ? "-" : "+"}
+        </button>
       </div>
     </div>
   );
 };
 
-const FaqSubtitle = ({ text }) => {
-  return <h2 className="font-inter w-3/4 text-xl font-medium">{text}</h2>;
-};
-
-const FaqButton = ({ open, clickOpen }) => {
-  return (
-    <div>
-      <button
-        onClick={clickOpen}
-        className="block flex h-10 w-10 items-center justify-center  rounded-full bg-primary-card text-xl"
-      >
-        {open ? <FaMinus /> : <FaPlus />}
-      </button>
-    </div>
-  );
-};
-
-const FaqAnswer = ({ open }) => {
-  return (
-    <div
-      className={`${
-        open ? "h-fit duration-300 ease-in-out" : "h-0 overflow-hidden"
-      } font-inter w-11/12 text-justify text-lg font-extralight md:w-3/4`}
-    >
-      <p>
-        Nulla lectus lectus, suscipit at posuere sit amet, imperdiet sit amet
-        sapien. Donec ornare odio justo, ac efficitur enim fermen. Nam enim
-        ligula, lacinia vitae venenatis quis, suscipit id augue.
-      </p>
-    </div>
-  );
-};
-
 const FAQSection = () => {
+  const [selectedAccordion, setSelectedAccordion] = useState("");
+
+  const handleSelectedAccordion = (currentAccordion) => {
+    setSelectedAccordion((prevAccordion) => {
+      if (prevAccordion === currentAccordion) {
+        return "";
+      }
+      return currentAccordion;
+    });
+  };
+
   return (
     <Layout
       bg="bg-white"
@@ -88,29 +56,32 @@ const FAQSection = () => {
     >
       <div className="space-y-14 text-center">
         <Heading type="h2" text="FAQs" />
-        <div className="text-left">
+        <div className="grid divide-y divide-[#E4E4E4] text-left">
           <FaqSection
-            isOpen={true}
+            isOpen={selectedAccordion === "one"}
+            handleOpen={() => handleSelectedAccordion("one")}
             title="01. Can I Find the right information faster?"
             description="Nulla lectus lectus, suscipit at posuere sit amet, imperdiet sit amet sapien. Donec ornare odio justo, ac efficitur enim fermen. Nam enim ligula, lacinia vitae venenatis quis, suscipit id augue."
           />
           <FaqSection
-            isOpen={false}
+            isOpen={selectedAccordion === "two"}
+            handleOpen={() => handleSelectedAccordion("two")}
             title="02. How to share feature demos and ideas?"
             description="Nulla lectus lectus, suscipit at posuere sit amet, imperdiet sit amet sapien. Donec ornare odio justo, ac efficitur enim fermen. Nam enim ligula, lacinia vitae venenatis quis, suscipit id augue."
           />
           <FaqSection
-            isOpen={false}
+            isOpen={selectedAccordion === "three"}
+            handleOpen={() => handleSelectedAccordion("three")}
             title="03. How to get insights from users?"
             description="Nulla lectus lectus, suscipit at posuere sit amet, imperdiet sit amet sapien. Donec ornare odio justo, ac efficitur enim fermen. Nam enim ligula, lacinia vitae venenatis quis, suscipit id augue."
           />
           <FaqSection
-            isOpen={false}
+            isOpen={selectedAccordion === "four"}
+            handleOpen={() => handleSelectedAccordion("four")}
             title="04. Can I develop my website without code?"
             description="Nulla lectus lectus, suscipit at posuere sit amet, imperdiet sit amet sapien. Donec ornare odio justo, ac efficitur enim fermen. Nam enim ligula, lacinia vitae venenatis quis, suscipit id augue."
           />
         </div>
-        {/* <FaqWrapper /> */}
       </div>
     </Layout>
   );

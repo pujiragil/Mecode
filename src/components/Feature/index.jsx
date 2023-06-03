447d54eee49520578c1b538711816d268bc4652f
pujiragil <ragilsg164@gmail.com>
2023-06-03 23:26:18 +0700

    change animation preview

--- src/components/Feature/index.jsx
+++ src/components/Feature/index.jsx
@@ -5,8 +5,14 @@ import Layout from "../Layout";
 import { SliderWrapper } from "./SliderElements";
 
 const FeatureSection = () => {
-  const ref = useRef(null);
-  const isInView = useInView(ref, { margin: "-100px", once: true });
+  const headingRef = useRef(null);
+  const isHeadingInView = useInView(headingRef, { amount: 0.5, once: true });
+
+  const paragraphRef = useRef(null);
+  const isParagraphInView = useInView(paragraphRef, {
+    amount: 0.5,
+    once: true,
+  });
 
   return (
     <Layout
@@ -14,24 +20,22 @@ const FeatureSection = () => {
       padding={["px-5", "py-10", "md:px-6", "md:py-12"]}
     >
       <div className="flex w-full flex-col gap-8 overflow-hidden md:flex-row md:items-center">
-        <div
-          ref={ref}
-          style={{
-            transform: isInView ? "none" : "translateX(-200px)",
-            opacity: isInView ? 1 : 0,
-            transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
-          }}
-          className="relative basis-full space-y-[18px]"
-        >
+        <div className="relative basis-full space-y-[18px]">
           <Heading
             type="h2"
+            ref={headingRef}
             widths={["w-full", "lg:w-10/12"]}
+            animate={`${
+              isHeadingInView ? "slide-in-x" : "slide-out-x"
+            } delay-300`}
             text="Our Features Special For You"
           />
           <Paragraph
-            isPrimary={false}
-            sizes={["text-base", "md:text-lg"]}
+            ref={paragraphRef}
             widths={["w-full"]}
+            animate={`${
+              isParagraphInView ? "slide-in-x" : "slide-out-x"
+            } delay-500`}
             text="We provide various special
             features for all of you"
           />
